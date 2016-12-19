cdd68916511e265cca21306b6c6ccc7ca8d95931
Sam <sam.goeman@hotmail.com>
2016-12-19 21:50:35 +0100

    Google maps

--- app/src/main/java/swarm_app_3/ehb/com/contour/MainActivity.java
+++ app/src/main/java/swarm_app_3/ehb/com/contour/MainActivity.java
@@ -1,7 +1,9 @@
 package swarm_app_3.ehb.com.contour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //ss
+    }
+
+    public void goToMaps(View view) {
+        Intent toMaps = new Intent(this, MapsActivity.class);
+        startActivity(toMaps);
     }
 }
---
+++ app/src/main/java/swarm_app_3/ehb/com/contour/MapsActivity.java
@@ -0,0 +1,85 @@
+package swarm_app_3.ehb.com.contour;
+
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    private LatLngBounds EUROPE = new LatLngBounds(
+            new LatLng(48.6908333333, 9.14055555556), new LatLng(48.6908333333, 9.14055555556));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng Gent = new LatLng(51.0535, 3.7304);
+        LatLng Biella = new LatLng(45.5630400, 8.0579600);
+
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(Gent, Biella)
+                .width(5)
+                .color(Color.GREEN));
+
+        mMap.setMinZoomPreference(6.0f);
+        mMap.setMaxZoomPreference(14.0f);
+
+
+
+// Set the camera to the greatest possible zoom level that includes the
+// bounds
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(EUROPE, 0));
+        try {
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+
+        }
+        /* Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+    }
+}
